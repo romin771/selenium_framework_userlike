dfcc14e7de63af169080f969a42e0e047ab4f7d7
romin771 <romin.parvardeh@gmail.com>
2023-10-09 21:55:04 +0200

    add conftests and refactor the login_tests file

--- tests/home/login_tests.py
+++ tests/home/login_tests.py
@@ -3,30 +3,23 @@ import unittest
 from pages.home.login_page import LoginPage
 import pytest
 
+@pytest.mark.usefixtures("oneTimeSetUp", "setUp")
+class LoginTests(unittest.TestCase):
 
-
-
-class LoginTests(unittest.TestCase ):
-
-    base_url = "https://www.userlike.com/en/"
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    # driver.get(base_url)
-    driver.implicitly_wait(3)
-    lp = LoginPage(driver)
+    @pytest.fixture(autouse=True) #class level setup
+    def classSetup(self, oneTimeSetUp):
+        self.lp = LoginPage(self.driver)
 
     @pytest.mark.run(order=2)
     def test_valid_login(self):
         self.lp.login("romin.parvardeh@gmail.com", "RominRomin!234!234")
         sussess_login = self.lp.verify_successful_login()
         assert sussess_login == True
-        self.driver.quit()
 
     #negative test case
     @pytest.mark.run(order=1)
     def test_invalid_login(self):
-        self.driver.get(self.base_url)
-
+        # self.driver.get(self.base_url)
         self.lp.login("romin.parvardeh@gmail.com", "RominRo4")
         sussess_login = self.lp.verify_fail_login()
         assert sussess_login == True

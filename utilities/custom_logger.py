722dae1fe683698b80010b889261b997cf28ec24
romin771 <romin.parvardeh@gmail.com>
2023-10-09 17:38:23 +0200

    created custom logger file to enhance our logs

---
+++ utilities/custom_logger.py
@@ -0,0 +1,20 @@
+import inspect
+import logging
+
+
+def customLogger(logLevel=logging.DEBUG): #defult value is DEBUG, so if we dont privide it from outside, we want everything gonna be log
+    # Gets the name of the class / method from where this method is called
+    loggerName = inspect.stack()[1][3]
+    logger = logging.getLogger(loggerName)
+    # By default, log all messages
+    logger.setLevel(logging.DEBUG)
+
+    fileHandler = logging.FileHandler("automation.log", mode='w') # if we want to override, we can change to 'w', append "a"
+    fileHandler.setLevel(logLevel)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s',
+                    datefmt='%m/%d/%Y %I:%M:%S %p')
+    fileHandler.setFormatter(formatter)
+    logger.addHandler(fileHandler)
+
+    return logger

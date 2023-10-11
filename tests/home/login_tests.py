c94290c9420d82261c04ec4164c1e9e6efb55353
romin771 <romin.parvardeh@gmail.com>
2023-10-11 15:12:39 +0200

    refactor login_tests based on testsstatus util file

--- tests/home/login_tests.py
+++ tests/home/login_tests.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 import unittest
 from pages.home.login_page import LoginPage
 import pytest
+from utilities.teststatus import TestStatus
 
 @pytest.mark.usefixtures("oneTimeSetUp", "setUp")
 class LoginTests(unittest.TestCase):
@@ -9,14 +10,17 @@ class LoginTests(unittest.TestCase):
     @pytest.fixture(autouse=True) #class level setup
     def classSetup(self, oneTimeSetUp):
         self.lp = LoginPage(self.driver)
+        self.ts = TestStatus(self.driver)
 
     @pytest.mark.run(order=2)
     def test_valid_login(self):
         self.lp.login("romin.parvardeh@gmail.com", "RominRomin!234!234")
         success_login = self.lp.verify_successful_login()
-        assert success_login == True
+        #assert success_login == True
+        self.ts.mark(success_login, "login was not successfull")
         title_verified = self.lp.verify_title()
-        assert title_verified == True
+        #assert title_verified == True
+        self.ts.markFinal("test_valid_login", title_verified, "logig wasn not successful")
 
 
     #negative test case
